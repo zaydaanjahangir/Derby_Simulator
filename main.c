7d0713470aa00d2c337499d79b0ce5b02332f855
Zaydaan Jahangir <154794199+zaydaanjahangir@users.noreply.github.com>
2024-07-17 16:51:06 -0400

    Implement dynamic odds with track biases

--- main.c
+++ main.c
@@ -7,16 +7,16 @@
 #include "sequences.h"
 
 #define NUM_HORSES 6 
-#define NUM_SIMULATIONS 1000
+#define NUM_SIMULATIONS 1000000
 
 
 Horse horses[NUM_HORSES] = {   
-    {"Thunderbolt", 8, 10, {"Johnny J.", 7}}, // 26
-    {"Whirlwind", 8, 10, {"Mickey P.", 7}},   // 28
-    {"Eclipse", 8, 10, {"Laura H.", 7}},       // 24
-    {"Comet", 8, 10, {"John S.", 7}},         // 23
-    {"Volcano", 8, 10, {"Hiton H.", 7}},       // 22
-    {"Monsoon", 8, 10, {"Mike M.", 7}}        // 26
+    {"Thunderbolt", 8, 10, {"Johnny J.", 7}, 0, 1, 0}, // 26
+    {"Whirlwind", 8, 10, {"Mickey P.", 7}, -1, 0, 1},  // 28
+    {"Eclipse", 8, 10, {"Laura H.", 7}, -1, -1, 1},    // 24
+    {"Comet", 8, 10, {"John S.", 7}, 1, 0, -1},         // 23
+    {"Volcano", 8, 10, {"Hiton H.", 7}, 0, 0, 1},       // 22
+    {"Monsoon", 8, 10, {"Mike M.", 10}, 1, 1, -1}        // 26
 };
 
 int main() {
@@ -38,6 +38,16 @@ int main() {
     if (isBetting) {        
         srand(time(NULL));  
         printf("\n");
+        TrackCondition trackCondition = rand() % 3;
+        calculateOdds(horses, odds, positions, trackCondition); 
+
+        // for loop helps debug the dynamic odds calculation
+
+        // for (int i = 0; i < NUM_HORSES; i++) {              
+        //     printf("%d. %s: %d:1\n", i + 1, horses[i].name, odds[i]);
+        // }
+        // return 0;
+
         startBetting(horses, odds, positions);  
     } else {displayResults(horses, positions);}
    
